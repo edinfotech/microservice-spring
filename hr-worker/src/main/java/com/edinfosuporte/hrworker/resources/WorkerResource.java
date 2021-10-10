c2db2bda92758ea39ed503f56d7882bc753055aa
Edivanio Da Silva Vieira <40584065+edinfotech@users.noreply.github.com>
2021-10-10 00:22:51 -0300

    private Git repository and add config server

--- hr-worker/src/main/java/com/edinfosuporte/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/edinfosuporte/hrworker/resources/WorkerResource.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,16 +16,27 @@ import org.springframework.web.bind.annotation.RestController;
 import com.edinfosuporte.hrworker.entities.Worker;
 import com.edinfosuporte.hrworker.repositories.WorkerRepository;
 
-
+@RefreshScope
 @RestController
 @RequestMapping(value = "/workers")
 public class WorkerResource {
+	
 	private static Logger  logger = org.slf4j.LoggerFactory.getLogger(WorkerResource.class);
+	
+	@Value("${test.config}")
+	private String testConfig;
 	@Autowired
 	private Environment  env;
 	
 	@Autowired
 	private WorkerRepository repository;
+	
+	
+	@GetMapping(value = "/config")
+	public ResponseEntity<Void> getConfigs(){
+		logger.info("CONFIG = " + testConfig);
+		return ResponseEntity.noContent().build();
+	}
 	
 	@GetMapping
 	public ResponseEntity<List<Worker>> findAllWorkers(){
