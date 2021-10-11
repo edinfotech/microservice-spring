4291f320a4c848dd3189ecf884a3fdc561a5c314
Edivanio Da Silva Vieira <40584065+edinfotech@users.noreply.github.com>
2021-10-10 22:58:27 -0300

    User,role,many to many association and seeding

---
+++ hr-user/src/test/java/com/edinfosuporte/hruser/HrUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edinfosuporte.hruser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
